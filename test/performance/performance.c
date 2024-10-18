fd1600489d8f1181bb8a1d96adb248a3563b914f
Sepi-ui <sebastian.tobler@hotmail.de>
2024-10-18 16:56:40 +0000

    Base for Performance testing

--- test/performance/performance.c
+++ test/performance/performance.c
@@ -2,6 +2,7 @@
 #include <assert.h>
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "ecs/component.h"
 #include "ecs/system.h"
 #include "ecs/entity.h"
@@ -18,13 +19,68 @@ typedef struct PerformanceTest {
 PerformanceTest tests[MAX_TESTS];
 int num_tests = 0;
 
-void register_test(int index, const char* name, void (*run)(int load, int iteration) {
-if (num_tests < MAX_TESTS) {
-	tests[num_test].index = index;
-	tests[num_test].name = name;
-	tests[num_test].run = run;
-	num_test++;
+void register_test(int index, const char* name, void (*run)(int load, int iteration)) {
+if (num_tests < MAX_TESTS) { 
+	tests[num_tests].index = index;
+	tests[num_tests].name = name;
+	tests[num_tests].run = run;
+	num_tests++;
 	}else{
-	error_log("Max number of tests reached"\n);
+	error_log("Max number of tests reached\n");
 	};
+}
+
+void run_selected_test(const char* tokenList, int load, int iteration) {
+char* token = strtok(tokenList, ",");
+if (token != NULL) {
+	int index = atoi(token);
+	for (int i = 0; i < num_tests; i++) {
+		if (tests[i].index == index) {
+			info_log("running %s with load: %d, iteration: %d",token[i].name, token[i].load, tests[i].iteration);
+			tests[i].run(load, iteration);
+
+			};
+		};
+	token = strtok(NULL, ",");
+
+
+	};
+}
+
+void register_all_tests() {
+	    register_test(1, "collision_test", collision_performance_test);
+	        register_test(2, "rendering_test", rendering_performance_test);
+		    register_test(3, "physics_test", physics_performance_test);
+}
+
+
+
+
+
+
+
+
+int main(int argc, char* argv[]) {
+	    if (argc < 4) {
+		info_log("Usage: %s <test_indices> <load> <iterations>\n", argv[0]);
+		return 1;
+		}
+
+	        const char* selected_tests = argv[1];
+		int load = atoi(argv[2]);
+	        int iterations = atoi(argv[3]);
+
+		// Register all performance tests
+		register_all_tests();
+
+		//List all available tests for user information
+		info_log("Available Tests:\n");
+		for (int i = 0; i < num_tests; i++) {
+		info_log("%d. %s\n", tests[i].index, tests[i].name);
+		}
+
+		// Run the selected tests
+		run_selected_test(selected_tests, load, iterations);
+
+		return 0;
 }
---
+++ test/performance/performanceFunctions.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "ecs/component.h"
+#include "ecs/system.h"
+#include "ecs/component.h"
+
+
+
+void rendering_performance_test(int load, int iteration) {
+return;
+}
+
+void collision_performance_test(int load, int iteration) {
+return;
+}
+
+void physics_performance_test(int load, int iteration) {
+return;
+}
